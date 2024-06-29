c9f6a9b0d0aec9e0c3f7bd49747937c88f1dcd8f
Daniel <thecodedaniel@gmail.com>
2024-06-29 11:22:53 +0100

    combine path and query params

--- myapi.py
+++ myapi.py
@@ -23,17 +23,24 @@ def index():
     return({'name' : 'first data'})
 
 
-# path params
+# using path params
 @app.get("/get-student/{student_id}")
 def get_student(student_id: int = Path(description="Out of range"), gt=0):
     return students[student_id]
 
-# query params
-@app.get("/get-by-name")
-def get_student(*, name: Optional[str] = None, test : int):
+# using query params
+# @app.get("/get-by-name")
+# def get_student(*, name: Optional[str] = None, test : int):
+#     for student_id in students:
+#         if students[student_id]['name'] == name:
+#            return students[student_id]
+#     return {"Data" : "Data Not found"}
+
+#combining path and query params
+
+@app.get("/get-by-name/{student_id}")
+def get_student(*, student_id: int, name: Optional[str] = None, test : int):
     for student_id in students:
         if students[student_id]['name'] == name:
            return students[student_id]
     return {"Data" : "Data Not found"}
-
-#combining path and query params
