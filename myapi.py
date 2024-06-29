fd3a12719a511a32a17df2cdbe6018718f2b31fb
Daniel <thecodedaniel@gmail.com>
2024-06-29 11:37:51 +0100

    post requests

--- myapi.py
+++ myapi.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Path
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -7,14 +8,19 @@ students = {
     1: {
         'name' : 'John',
         'age' : '17',
-        'class' : 'year 12'
+        'year' : 'year 12'
     },
     2: {
         'name' : 'Jane',
         'age' : '15',
-        'class' : 'year 10'
+        'year' : 'year 10'
     }
 }
+
+class Student(BaseModel):
+    name: str
+    age: int
+    year: str
 
 
 # default index url
@@ -29,18 +35,28 @@ def get_student(student_id: int = Path(description="Out of range"), gt=0):
     return students[student_id]
 
 # using query params
-# @app.get("/get-by-name")
-# def get_student(*, name: Optional[str] = None, test : int):
-#     for student_id in students:
-#         if students[student_id]['name'] == name:
-#            return students[student_id]
-#     return {"Data" : "Data Not found"}
+@app.get("/get-by-name")
+def get_student(*, name: Optional[str] = None, test : int):
+    for student_id in students:
+        if students[student_id]['name'] == name:
+           return students[student_id]
+    return {"Data" : "Data Not found"}
+
 
 #combining path and query params
-
 @app.get("/get-by-name/{student_id}")
 def get_student(*, student_id: int, name: Optional[str] = None, test : int):
     for student_id in students:
         if students[student_id]['name'] == name:
            return students[student_id]
     return {"Data" : "Data Not found"}
+
+
+# post request
+@app.post("/create-student/{student_id}")
+def create_student(student_id: int, student: Student):
+    if student_id in students:
+        return {"error" : "student exists"}
+    
+    students[student_id] = student
+    return students
